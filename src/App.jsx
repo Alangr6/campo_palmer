cc296bd52cd570e35be4b1bf587927f2498e74ee
alangr6 <alanguerrero623@gmail.com>
2023-02-22 22:52:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <main className='title-main'>
+        <h1 className='page-title'>Venta de palmeras Almeria - Campo de Níjar</h1>
+        <h1>Campo Palmer</h1>
+      </main>
+    </div>
+  )
+}
+
+export default App
