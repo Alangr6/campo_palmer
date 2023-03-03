174d873140646616c52423c6ae8bb7813456789d
Jorge Ramírez Galván <jorgeramirezgalvan99@gmail.com>
2023-03-03 13:01:58 +0100

    navbar changes and 1 sec added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import logo from "./images/logo.svg";
-import mainsec from "./images/trans.png";
+import mainsec from "./images/main-sec1.png";
 import palm from "./images/palm1.png";
+import junglepalm from "./images/sec3.png";
+import junglepalm2 from "./images/sec2.jpg";
 import "./App.css";
 
 function App() {
@@ -9,34 +11,40 @@ function App() {
     <div className="App">
       <header>
         <nav className="navbar">
+          <div className="box-title">
+            <h1 className="company-name">Palmer</h1>
+          </div>
           <div className="box-logo">
             <img src={logo} className="logo" alt="" />
           </div>
           <div className="box-title">
-            <h1 className="company-name">Palmer Camp</h1>
+            <h1 className="company-name">Camp</h1>
           </div>
         </nav>
       </header>
       <main>
         <section className="main-sec">
-          <div className="page-title-box">
-            <h1>Venta de palmeras Almeria</h1>
-            <h1>-</h1>
-            <h1>Campo de Níjar</h1>
+          <div className="main-image-left">
+            <img src={mainsec} className="main-sec-img" alt="" />
           </div>
-          <img src={mainsec} className="main-sec-img" alt="" />
+          <div className="page-title-box">
+            <h1>VENTA DE PALMERAS ALMERÍA</h1>
+            <h1>-</h1>
+            <h1>CAMPO DE NÍJAR</h1>
+          </div>
+          <div className="main-image-right">
+            <img src={mainsec} className="main-sec-img" alt="" />
+          </div>
         </section>
-      </main>
-      <main>
         <section className="first-sec">
-          <h2 className="box-sec-title">CRIADAS ECOLOGICAMENTE</h2>
+          <h2 className="box-sec-title">CRIADAS ECOLÓGICAMENTE</h2>
           <div className="card-box">
             <div className="card">
               <img src={palm} className="card-image" alt="" />
               <div className="card-text">
                 <h3 className="card-title">SMALL PLANTS</h3>
                 <div className="card-separator"></div>
-                <p className="card-text">
+                <p>
                   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
                   maiores eum beatae soluta corrupti quibusdam consectetur
                   repellat facilis? Voluptatem
@@ -48,7 +56,7 @@ function App() {
               <div className="card-text">
                 <h3 className="card-title">SMALL PLANTS</h3>
                 <div className="card-separator"></div>
-                <p className="card-text">
+                <p>
                   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
                   maiores eum beatae soluta corrupti quibusdam consectetur
                   repellat facilis? Voluptatem
@@ -60,13 +68,42 @@ function App() {
               <div className="card-text">
                 <h3 className="card-title">SMALL PLANTS</h3>
                 <div className="card-separator"></div>
-                <p className="card-text">
+                <p>
                   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
                   maiores eum beatae soluta corrupti quibusdam consectetur
                   repellat facilis? Voluptatem
                 </p>
               </div>
             </div>
+          </div>
+        </section>
+        <section className="about-sec">
+          <div className="image-box-about-sec">
+            <img src={junglepalm} className="about-sec-image" alt="" />
+          </div>
+          <div className="about-box">
+            <h2 className="about-title">ABOUT US</h2>
+            <p>
+              Bienvenidos a Campo Palmer, una empresa líder en la producción de
+              palmeras ecológicas de alta calidad. Estamos comprometidos con la
+              creación de productos de primera categoría, cultivados de manera
+              responsable y sostenible en armonía con el medio ambiente.
+            </p>
+            <p>
+              Desde nuestros inicios, hemos trabajado arduamente para
+              establecernos como un referente en la industria agrícola,
+              destacando por la calidad de nuestros productos y el respeto que
+              tenemos por la naturaleza. Utilizamos técnicas agrícolas
+              innovadoras y sostenibles para asegurarnos de que nuestras
+              palmeras crezcan en un ambiente saludable y natural.
+            </p>
+            <p>
+              Gracias a nuestra dedicación y compromiso con la calidad y el
+              medio ambiente, hemos logrado ganar la confianza y lealtad de
+              nuestros clientes en todo el mundo. En Campo Palmer, nos
+              enorgullece ser una empresa que se preocupa por el medio ambiente
+              y por crear productos sostenibles que beneficien a todos.
+            </p>
           </div>
         </section>
       </main>
