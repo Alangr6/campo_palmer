5b16c2ac022781354aa6b9f6bdb3e9dd0be4dea6
Jorge Ramírez Galván <jorgeramirezgalvan99@gmail.com>
2023-03-05 15:11:19 +0100

    Contact form

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,12 @@ function App() {
             <h1 className="company-name">Palmer</h1>
           </div>
           <div className="box-logo">
-            <img src={logo} className="logo" title="Palmer Camp logo" alt="White and transparen palm tree" />
+            <img
+              src={logo}
+              className="logo"
+              title="Palmer Camp logo"
+              alt="White and transparen palm tree"
+            />
           </div>
           <div className="box-title">
             <h1 className="company-name">Camp</h1>
@@ -42,7 +47,12 @@ function App() {
       <main>
         <section className="main-sec">
           <div className="main-image-left">
-            <img src={mainsec} className="main-sec-img" title="Palm tree leaf"  alt="Palm tree leaf, envolving the page title" />
+            <img
+              src={mainsec}
+              className="main-sec-img"
+              title="Palm tree leaf"
+              alt="Palm tree leaf, envolving the page title"
+            />
           </div>
           <div className="page-title-box">
             <h1>VENTA DE PALMERAS ALMERÍA</h1>
@@ -50,14 +60,24 @@ function App() {
             <h1>CAMPO DE NÍJAR</h1>
           </div>
           <div className="main-image-right">
-            <img src={mainsec} className="main-sec-img" title="Palm tree leaf" alt="Palm tree leaf, envolving the page title" />
+            <img
+              src={mainsec}
+              className="main-sec-img"
+              title="Palm tree leaf"
+              alt="Palm tree leaf, envolving the page title"
+            />
           </div>
         </section>
         <section className="first-sec">
           <h2 className="box-sec-title reveal">CRIADAS ECOLÓGICAMENTE</h2>
           <div className="card-box">
             <div className="card reveal">
-              <img src={palm} className="card-image" title="Palm trees farm" alt="Palm trees growth" />
+              <img
+                src={palm}
+                className="card-image"
+                title="Palm trees farm"
+                alt="Palm trees growth"
+              />
               <div className="card-text">
                 <h3 className="card-title reveal">BIO FARM</h3>
                 <div className="card-separator reveal"></div>
@@ -69,7 +89,12 @@ function App() {
               </div>
             </div>
             <div className="card reveal">
-              <img src={palm2} className="card-image" title="Not pruned palm tree" alt="Not pruned palm tree growth" />
+              <img
+                src={palm2}
+                className="card-image"
+                title="Not pruned palm tree"
+                alt="Not pruned palm tree growth"
+              />
               <div className="card-text">
                 <h3 className="card-title reveal">GROWTH</h3>
                 <div className="card-separator reveal"></div>
@@ -81,7 +106,12 @@ function App() {
               </div>
             </div>
             <div className="card reveal">
-              <img src={palm3} className="card-image" title="Pruned palm" alt="Pruned palm tree, final product" />
+              <img
+                src={palm3}
+                className="card-image"
+                title="Pruned palm"
+                alt="Pruned palm tree, final product"
+              />
               <div className="card-text">
                 <h3 className="card-title reveal">FINAL PRODUCT</h3>
                 <div className="card-separator reveal"></div>
@@ -121,12 +151,22 @@ function App() {
             </p>
           </div>
           <div className="image-box-width">
-            <img src={worksecimg} className="sec-image" title="Work section palm tree" alt="Palm tree work section image" />
+            <img
+              src={worksecimg}
+              className="sec-image"
+              title="Work section palm tree"
+              alt="Palm tree work section image"
+            />
           </div>
         </section>
-        <section className="sec about-sec">
+        <section className="sec">
           <div className="image-box-width">
-            <img src={junglepalm} className="sec-image" title="About section palm tree" alt="Palm tree about section image" />
+            <img
+              src={junglepalm}
+              className="sec-image"
+              title="About section palm tree"
+              alt="Palm tree about section image"
+            />
           </div>
           <div className="sec-box image-box-width">
             <h2 className="sec-title reveal">NOSOTROS</h2>
@@ -155,12 +195,17 @@ function App() {
         </section>
       </main>
       <footer className="contact-sec">
-        <div className="contact-sec-title">
-          <h2>CONTACT US</h2>
-        </div>
-        <div className="number-box">
-          <h3>+34 673 670 067</h3>
-        </div>
+        <h2>CONTACT US!</h2>
+        <form
+          action="https://formsubmit.co/your@email.com"
+          method="POST"
+        >
+          <input type="text" name="name" id="" placeholder="Name" required />
+          <input type="email" name="email" id="" placeholder="Email" required />
+          <input type="phone" name="phone" id="" placeholder="Phone" required />
+          <button type="submit">Submit</button>
+        </form>
+        <h3>+34 673 670 067</h3>
       </footer>
     </div>
   );
