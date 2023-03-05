79765f262afc3c82f64b8ae27c98877dd71866f0
Jorge Ramírez Galván <jorgeramirezgalvan99@gmail.com>
2023-03-05 13:15:26 +0100

    work and contact sec

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from "react";
 import logo from "./images/logo.svg";
 import mainsec from "./images/main-sec1.png";
-import palm from "./images/palm1.png";
+import palm from "./images/palm12.png";
+import palm2 from "./images/palm5.png";
+import palm3 from "./images/palm1.png";
 import junglepalm from "./images/sec3.png";
-import junglepalm2 from "./images/sec2.jpg";
+import junglepalm2 from "./images/sec2.jpg"; /**probar esta foto*/
+import worksecimg from "./images/work-sec.png";
 import "./App.css";
 
 function App() {
+  window.addEventListener("scroll", reveal);
+
+  function reveal() {
+    let reveals = document.querySelectorAll(".reveal");
+
+    for (let i = 0; i < reveals.length; i++) {
+      let windowheight = window.innerHeight;
+      let revealtop = reveals[i].getBoundingClientRect().top;
+      let revealpoint = 150;
+
+      if (revealtop < windowheight - revealpoint) {
+        reveals[i].classList.add("active");
+      }
+    }
+  }
+  reveal();
   return (
     <div className="App">
       <header>
@@ -37,59 +55,89 @@ function App() {
           </div>
         </section>
         <section className="first-sec">
-          <h2 className="box-sec-title">CRIADAS ECOLÓGICAMENTE</h2>
+          <h2 className="box-sec-title reveal">CRIADAS ECOLÓGICAMENTE</h2>
           <div className="card-box">
-            <div className="card">
+            <div className="card reveal">
               <img src={palm} className="card-image" alt="" />
               <div className="card-text">
-                <h3 className="card-title">SMALL PLANTS</h3>
-                <div className="card-separator"></div>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
-                  maiores eum beatae soluta corrupti quibusdam consectetur
-                  repellat facilis? Voluptatem
+                <h3 className="card-title reveal">BIO FARM</h3>
+                <div className="card-separator reveal"></div>
+                <p className="reveal">
+                  Creemos que la naturaleza tiene su propia belleza y, por lo
+                  tanto, cultivamos palmeras sin podar para mantener su aspecto
+                  auténtico y natural.
                 </p>
               </div>
             </div>
-            <div className="card">
-              <img src={palm} className="card-image" alt="" />
+            <div className="card reveal">
+              <img src={palm2} className="card-image" alt="" />
               <div className="card-text">
-                <h3 className="card-title">SMALL PLANTS</h3>
-                <div className="card-separator"></div>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
-                  maiores eum beatae soluta corrupti quibusdam consectetur
-                  repellat facilis? Voluptatem
+                <h3 className="card-title reveal">GROWTH</h3>
+                <div className="card-separator reveal"></div>
+                <p className="reveal">
+                  Nuestro producto final son las hermosas palmeras podadas, que
+                  han sido cuidadosamente moldeadas por expertos para resaltar
+                  su belleza natural.
                 </p>
               </div>
             </div>
-            <div className="card">
-              <img src={palm} className="card-image" alt="" />
+            <div className="card reveal">
+              <img src={palm3} className="card-image" alt="" />
               <div className="card-text">
-                <h3 className="card-title">SMALL PLANTS</h3>
-                <div className="card-separator"></div>
-                <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
-                  maiores eum beatae soluta corrupti quibusdam consectetur
-                  repellat facilis? Voluptatem
+                <h3 className="card-title reveal">FINAL PRODUCT</h3>
+                <div className="card-separator reveal"></div>
+                <p className="reveal">
+                  Palmeras elegantes y saludables, ideales para paisajes y
+                  jardines, agrega un toque de elegancia y belleza natural a tu
+                  hogar o negocio.
                 </p>
               </div>
             </div>
           </div>
         </section>
-        <section className="about-sec">
-          <div className="image-box-about-sec">
-            <img src={junglepalm} className="about-sec-image" alt="" />
+        <section className="sec work-color">
+          <div className="sec-box work-text-box-width">
+            <h2 className="sec-title work-color reveal">WORK</h2>
+            <p className="reveal">
+              Palmera de aceite: También conocida como palma de aceite africana,
+              es una especie muy valiosa que produce un aceite rico en
+              nutrientes y ampliamente utilizado en la industria alimentaria.
+              Nuestras palmeras de aceite se cultivan de manera sostenible y
+              ecológica, garantizando la calidad y la pureza del aceite
+              producido.
+            </p>
+            <p className="reveal">
+              Palmera datilera: Originaria del norte de África y el Medio
+              Oriente, la palmera datilera produce frutos dulces y sabrosos
+              conocidos como dátiles. En Campo Palmer, cultivamos palmeras
+              datileras de alta calidad, garantizando que nuestros dátiles sean
+              deliciosos y saludables.
+            </p>
+            <p className="reveal">
+              Palmera de coco: La palmera de coco es una especie versátil que
+              produce una variedad de productos, desde coco rallado hasta aceite
+              de coco y agua de coco. En Campo Palmer, nuestras palmeras de coco
+              se cultivan de manera ecológica y sostenible, garantizando que
+              nuestros productos de coco sean frescos y de alta calidad.
+            </p>
           </div>
-          <div className="about-box">
-            <h2 className="about-title">ABOUT US</h2>
-            <p>
+          <div className="image-box-width">
+            <img src={worksecimg} className="sec-image" alt="" />
+          </div>
+        </section>
+        <section className="sec about-sec">
+          <div className="image-box-width">
+            <img src={junglepalm} className="sec-image" alt="" />
+          </div>
+          <div className="sec-box image-box-width">
+            <h2 className="sec-title reveal">NOSOTROS</h2>
+            <p className="reveal">
               Bienvenidos a Campo Palmer, una empresa líder en la producción de
               palmeras ecológicas de alta calidad. Estamos comprometidos con la
               creación de productos de primera categoría, cultivados de manera
               responsable y sostenible en armonía con el medio ambiente.
             </p>
-            <p>
+            <p className="reveal">
               Desde nuestros inicios, hemos trabajado arduamente para
               establecernos como un referente en la industria agrícola,
               destacando por la calidad de nuestros productos y el respeto que
@@ -97,7 +145,7 @@ function App() {
               innovadoras y sostenibles para asegurarnos de que nuestras
               palmeras crezcan en un ambiente saludable y natural.
             </p>
-            <p>
+            <p className="reveal">
               Gracias a nuestra dedicación y compromiso con la calidad y el
               medio ambiente, hemos logrado ganar la confianza y lealtad de
               nuestros clientes en todo el mundo. En Campo Palmer, nos
@@ -107,6 +155,14 @@ function App() {
           </div>
         </section>
       </main>
+      <footer className="contact-sec">
+        <div className="contact-sec-title">
+          <h2>CONTACT US</h2>
+        </div>
+        <div className="number-box">
+          <h3>+34 673 670 067</h3>
+        </div>
+      </footer>
     </div>
   );
 }
